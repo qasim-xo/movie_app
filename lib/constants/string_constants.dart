5d8a2e6f7eb8fe1d32830d6b98d55bcb1bf41589
qasim-xo <143028673+qasim-xo@users.noreply.github.com>
2025-02-05 23:39:26 +0500

    Remove string_constants.dart from version control

--- lib/constants/string_constants.dart
+++
@@ -1,9 +0,0 @@
-const String apiKey = "bb81092569cb8af64b46cce9228fdc0e";
-const String baseUrl = "https://api.themoviedb.org/3";
-
-class AssetIcons {
-  static const String homeIcon = "assets/icons/home.png";
-  static const String tvShowIcon = "assets/icons/tv.png";
-  static const String movieIcon = "assets/icons/video-camera.png";
-  static const String aiIcon = "assets/icons/ai.png";
-}

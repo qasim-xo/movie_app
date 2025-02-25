ef484d0c857893632730ee6b9614edcba5d06fa9
qasim-xo <143028673+qasim-xo@users.noreply.github.com>
2025-02-25 22:52:01 +0500

    fetched job data from api

--- lib/features/director_info/screens/mobile/director_info_mobile_screen.dart
+++ lib/features/director_info/screens/mobile/director_info_mobile_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:movie_app/features/director_info/providers/director_provider.dart';
+import 'package:movie_app/models/combined_credits_crew/combined_credits_crew.dart';
 
 class DirectorInfoMobileScreen extends ConsumerWidget {
   const DirectorInfoMobileScreen({super.key});
@@ -13,17 +14,42 @@ class DirectorInfoMobileScreen extends ConsumerWidget {
     return DefaultTabController(
       length: jobList.length,
       child: Scaffold(
-          appBar: AppBar(
-            title: const Text('Person'),
-          ),
-          body: TabBar(
-            tabs: [
-              for (var job in jobList)
-                Tab(
-                  text: job,
-                )
-            ],
-          )),
+        appBar: AppBar(
+          title: const Text('Person'),
+        ),
+        body: Column(
+          children: [
+            TabBar(
+              tabAlignment: TabAlignment.start,
+              isScrollable: true,
+              tabs: [
+                for (var job in jobList)
+                  Tab(
+                    text: job,
+                  )
+              ],
+            ),
+            Expanded(
+              child: TabBarView(
+                children: jobList.map((job) {
+                  List filteredList =
+                      combinedCreditsCrew.where((e) => e.job == job).toList();
+
+                  return ListView.builder(
+                    shrinkWrap: true,
+                    physics:
+                        const NeverScrollableScrollPhysics(), // Prevents nested scroll issues
+                    itemCount: filteredList.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Text(filteredList[index].title);
+                    },
+                  );
+                }).toList(),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
